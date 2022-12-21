6a8b2f14a49f7aab08bdf861e54f6d2dc58430f7
redotter <arthemy.ryzhov@gmail.com>
2022-12-21 09:49:57 +0100

    add buttons

--- bot_config.py
+++ bot_config.py
@@ -1,11 +1,18 @@
 import logging
-from aiogram import Bot
-from aiogram.dispatcher import Dispatcher
-from config import TOKEN
+import os
 
+from aiogram import Bot
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import Dispatcher
+
+token_var = 'TG_BOT_TOKEN'
+TOKEN = os.environ.get(token_var)
+if TOKEN is None:
+    print(f'You need to specify {token_var}')
+    exit(1)
 
 bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=MemoryStorage())
 
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger('broadcast')
