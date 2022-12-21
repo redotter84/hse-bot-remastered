d5ea53816e18f53ebc0d6a0dcc2d0c52ec55993f
redotter <arthemy.ryzhov@gmail.com>
2022-12-21 11:43:05 +0100

    add mute buttons

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 from typing import List, Optional
 
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine, Boolean, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -18,9 +18,7 @@ class TgSubscription(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     user_id = Column(Integer)
     chat_id = Column(Integer)
-
-    def __repr__(self) -> str:
-        return f'<database.TgSubscription id: {self.id}, user_id: {self.user_id}, chat_id: {self.chat_id}>'
+    muted = Column(Boolean)
 
 
 class SheetSubscription(Base):
@@ -30,9 +28,7 @@ class SheetSubscription(Base):
     user_id = Column(Integer)
     sheet_link = Column(String(length=150))
     sheet_range = Column(String(length=16))
-
-    def __repr__(self) -> str:
-        return f'<database.SheetsSubscription id: {self.id}, user_id: {self.user_id}, sheet_link: {self.sheet_link}, range: {self.sheet_range}>'
+    muted = Column(Boolean)
 
 
 # ----------  TG  ----------
@@ -49,10 +45,13 @@ def create_tg_subscription(user_id: int, chat_id: int) -> TgSubscription:
     return subscription
 
 
-def get_tg_subscription(user_id: int, chat_id: int) -> Optional[TgSubscription]:
+def get_tg_subscriptions(user_id: int, chat_id: int):
     return session.query(TgSubscription)\
-        .filter((TgSubscription.user_id == user_id) & (TgSubscription.chat_id == chat_id))\
-        .first()
+        .filter((TgSubscription.user_id == user_id) & (TgSubscription.chat_id == chat_id))
+
+
+def get_tg_subscription(user_id: int, chat_id: int) -> Optional[TgSubscription]:
+    return get_tg_subscriptions(user_id, chat_id).first()
 
 
 def get_tg_subscriptions_by_user(user_id: int) -> List[TgSubscription]:
@@ -72,6 +71,15 @@ def remove_tg_subscription(user_id: int, chat_id: int) -> None:
     session.commit()
 
 
+def toggle_mute_for_tg_subscription(user_id: int, chat_id) -> None:
+    subscription = get_tg_subscription(user_id=user_id, chat_id=chat_id)
+    if subscription is None:
+        raise ValueError(f'Subscription with user_id={user_id} and chat_id={chat_id} does not exist.')
+
+    get_tg_subscriptions(user_id, chat_id).update({'muted': not subscription.muted})
+    session.commit()
+
+
 # ----------  Sheets ----------
 
 
@@ -86,12 +94,15 @@ def create_sheet_subscription(user_id: int, sheet_link: str, sheet_range: str) -
     return subscription
 
 
-def get_sheet_subscription(user_id: int, sheet_link: str, sheet_range: str) -> Optional[SheetSubscription]:
+def get_sheet_subscriptions(user_id: int, sheet_link: str, sheet_range: str):
     return session.query(SheetSubscription)\
         .filter((SheetSubscription.user_id == user_id)
                 & (SheetSubscription.sheet_link == sheet_link)
                 & (SheetSubscription.sheet_range == sheet_range))\
-        .first()
+
+
+def get_sheet_subscription(user_id: int, sheet_link: str, sheet_range: str) -> Optional[SheetSubscription]:
+    return get_sheet_subscriptions(user_id, sheet_link, sheet_range).first()
 
 
 def get_sheet_subscriptions_by_user_id(user_id: int) -> List[SheetSubscription]:
@@ -112,6 +123,15 @@ def delete_sheet_subscription(user_id: int, sheet_link: str, sheet_range: str) -
         raise ValueError(f"Sheet subscription with user_id={user_id} and sheet_link={sheet_link} and sheet_range={sheet_range} does not exist.")
 
     session.delete(subscription)
+    session.commit()
+
+
+def toggle_mute_for_sheet_subscription(user_id: int, sheet_link: str, sheet_range: str) -> None:
+    subscription = get_sheet_subscription(user_id=user_id, sheet_link=sheet_link, sheet_range=sheet_range)
+    if subscription is None:
+        raise ValueError(f'Subscription with user_id={user_id} and chat_id={chat_id} does not exist.')
+
+    get_sheet_subscriptions(user_id=user_id, sheet_link=sheet_link, sheet_range=sheet_range).update({'muted': not subscription.muted})
     session.commit()
 
 
